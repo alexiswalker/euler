0571776053292c2b0148a2d73ac2485eca2e4b0b
alexis <alexiswalker@hotmail.com>
2016-04-23 19:00:35 -0300

    digitos recursivos

--- 26.py
+++ 26.py
@@ -1,1 +1,25 @@
-print 'hola!'
+import math
+from fractions import Fraction
+
+##print map(lambda x: format(1.0/x, '.1000f'), numbers)
+
+l = []
+
+def repeating(numerator, denominator):
+    n = numerator*10
+    
+    if l.__contains__(n):
+        if n == 0:
+            return 0
+        else:
+            return len(l) - l.index(n)
+    else:
+        l.append(n)
+        repeating(n%denominator, denominator)
+
+r = []
+    
+for i in range(2, 1001):
+    l = []
+    print repeating(1,i)
+    
