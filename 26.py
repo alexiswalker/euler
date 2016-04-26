60d3b3f46b6878871dac8dd317c98ccc48d9077d
alexis <alexiswalker@hotmail.com>
2016-04-25 23:35:42 -0300

    ajuste arreglos en la recursion

--- 26.py
+++ 26.py
@@ -3,24 +3,26 @@ from fractions import Fraction
 
 ##print map(lambda x: format(1.0/x, '.1000f'), numbers)
 
-l = []
 
-def repeating(numerator, denominator):
+def repeating(numerator, denominator, repeating_part=None):
+    
+    if repeating_part is None:
+        repeating_part = []
+        
     n = numerator*10
     
-    if l.__contains__(n):
+    if repeating_part.__contains__(n):
         if n == 0:
             return 0
         else:
-            return len(l) - l.index(n)
+            return len(repeating_part) - repeating_part.index(n)
     else:
-        l.append(n)
-        return repeating(n%denominator, denominator)
+        repeating_part.append(n)
+        return repeating(n%denominator, denominator, repeating_part)
 
 r = []
     
 for i in range(2, 1000):
-    l = []
     r.append (repeating(1,i))
     
 print max(r), r.index(max(r))+2
--- 74.py
+++ 74.py
@@ -3,18 +3,21 @@ from math import factorial
 def suma_factorial_digitos(numero):
     return sum(map(factorial,map(int,str(numero))))
 
-l=[]
-def terminos_no_repetidos(numero):
-    if l.__contains__(numero):
-        return l
+def terminos_no_repetidos(numero, terminos=None):
+    if terminos is None:
+        terminos = []
+        
+    if terminos.__contains__(numero):
+        return terminos
     else:
-        l.append(numero) 
+        terminos.append(numero) 
         suma = suma_factorial_digitos(numero)
-        return terminos_no_repetidos(suma)
+        return terminos_no_repetidos(suma, terminos)
+
+print terminos_no_repetidos(69)
 
 c=0
-for i in range(1, 10**6):
-    l=[]
+for i in range(1, 10**3):
     if len(terminos_no_repetidos(i)) == 60:
         c=c+1
         
