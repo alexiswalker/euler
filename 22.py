8b529d74698cd45ed859ee749f86908a28a5a387
alexis <alexiswalker@hotmail.com>
2016-06-12 20:47:03 -0300

    ejercicio 22

---
+++ 22.py
@@ -0,0 +1,21 @@
+def letters():
+    return { chr(i) : i-64 for i in range(65, 91)}
+
+def names():
+    f = open('C:\Users\Alexis\Desktop\p022_names.txt', 'r')
+    names = f.read().replace('"', '')
+    f.close()
+    return sorted(names.split(','))
+
+def position():
+    n = names()
+    return { n[i] : i+1 for i in range(len(n))}
+
+
+
+l = letters()
+p = position()
+
+print sum([p[name] * sum([l[c] for c in name]) for name in names()])
+    
+       
