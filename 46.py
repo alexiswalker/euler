db66b62b178df895126ba4eb9f3d4099e3bbd447
alexis <alexiswalker@hotmail.com>
2016-06-18 19:45:26 -0300

    46

--- 46.py
+++ 46.py
@@ -1,4 +1,4 @@
-from euler_util import primes_sieve, is_prime
+from euler_util import primes_sieve, is_prime, is_odd
 from math import sqrt
 
 
@@ -6,21 +6,5 @@ from math import sqrt
 def is_sum_of_a_prime_and_twice_a_square(number):
     return len([i for i in primes_sieve(number - 2) if sqrt((number - i)/2.0).is_integer()]) > 0
 
-c = 7
+print [i for i in range(1, 10000) if is_odd(i) and not is_prime(i) and not is_sum_of_a_prime_and_twice_a_square(i)]
 
-while True:
-    
-    c = c + 2 
-    
-    if is_prime(c):
-        continue
-    
-    if not is_sum_of_a_prime_and_twice_a_square(c):
-        exit
-    
-print c
-
-
-#sqrt((number - i)/2)
-
-#[2, 3, 5, 7]
