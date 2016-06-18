bc32d0759f11ce16b9bd31a6c643826e476bea47
alexis <alexiswalker@hotmail.com>
2016-06-18 00:08:57 -0300

    23

--- 23.py
+++ 23.py
@@ -9,4 +9,4 @@ combinations = combinations_with_replacement(abundants, 2)
 
 numbers_sum_abundant = set([i for i in combinations_with_replacement(abundants, 2) if sum(i) < sum_of_two_abundant_numbers])
 
-print sum(set(rrange(1, sum_of_two_abundant_numbers)).difference(numbers_sum_abundant))
+print sum(set(range(1, sum_of_two_abundant_numbers)).difference(numbers_sum_abundant))
