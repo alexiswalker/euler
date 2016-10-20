19a78fd53e77dc95216b82799f02e4b67353d638
alexis <alexiswalker@hotmail.com>
2016-10-20 17:20:40 -0300

    ejercicio 67 tdd

--- 67_test.py
+++ 67_test.py
@@ -10,6 +10,12 @@ class Test(unittest.TestCase):
     def test_obtener_datos_desde_archivo(self):
         self.assertEqual(self.datos, [[3],[7,4],[2,4,6],[8,5,9,3]])
 
+    def test_obtener_coordenada_elemento_izquierdo(self):
+        self.assertEqual(coordenada_izquierdo(2,0) ,(3,0))
+
+    def test_obtener_coordenada_elemento_derecho(self):
+        self.assertEqual(coordenada_derecho(2,0), (3,1))
+
 
 if __name__ == '__main__':
     unittest.main()
--- e67.py
+++ e67.py
@@ -4,7 +4,26 @@ def obtener_datos_desde_archivo(path):
         file = f.readlines()
 
     return [map(int, i.split(' ')) for i in file]
+
+def coordenada_izquierdo(fila, columna):
+    return (fila+1, columna)
+
+def coordenada_derecho(fila, columna):
+    return (fila+1, columna+1)
+    
         
 if __name__ == '__main__':
-    print obtener_datos_desde_archivo('p067_triangle.txt')     
-  
+    datos = obtener_datos_desde_archivo('p067_triangle.txt')     
+    for i in range(datos.__len__()-1)[::-1]:
+        for j in range(datos[i].__len__()):
+            coord = coordenada_izquierdo(i, j)
+            valor_izquierdo = datos[coord[0]][coord[1]]
+
+            coord = coordenada_derecho(i,j)
+            valor_derecho = datos[coord[0]][coord[1]]
+
+            padre = datos[i][j]
+
+            datos[i][j]=max(padre + valor_derecho, padre + valor_izquierdo)
+    
+    print datos[0][0]
